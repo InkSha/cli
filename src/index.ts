b1f2702849e1b6e28f996576f209bef3e22bfc1f
InkSha <x1a010@163.com>
2024-06-21 18:09:39 +0800

    feat: bin command config

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,27 @@
-import packageFile from './../package.json'  assert { type: 'json' }
-import figlet from 'figlet'
+#!/usr/bin/env node
 
-console.log(figlet.textSync(packageFile.name))
+import { Command } from 'commander'
+import packageFile from './../package.json'  assert { type: 'json' }
+// import figlet from 'figlet'
+
+const program = new Command()
+  .name(packageFile.name)
+  .description(packageFile.description)
+  .version(packageFile.version)
+
+console.clear()
+// console.log(figlet.textSync(cliName))
+
+program
+  .command('init <project>')
+  .action(project => {
+    console.log(`you input project name is ${project}`)
+  })
+
+program
+  .command('update <project>')
+  .action(project => {
+    console.log(`you need update project is ${project}`)
+  })
+
+program.parse()
